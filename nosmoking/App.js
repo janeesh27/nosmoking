c3b0c5307c8ffc65af482ed9c9cbe48a169bbd33
janeesh <janeesh.tin@gmail.com>
2022-10-29 01:18:14 +0530

    media

--- nosmoking/App.js
+++ nosmoking/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>hello world!</Text>
-      <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+   fontcolor:'white',
   },
 });
